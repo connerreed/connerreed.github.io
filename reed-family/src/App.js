82bf90ee6ceefbb897ee29606f977ef1f07b86e0
Conner Reed <98499861+connerreed@users.noreply.github.com>
2023-11-24 21:14:19 -0600

    gg

--- reed-family/src/App.js
+++ reed-family/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>App.js</code> and save to reload.
+           <code>gfsdApp.js</code> and save to reload.
         </p>
         <a
           className="App-link"
