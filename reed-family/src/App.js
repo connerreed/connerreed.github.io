89caae74070070dd4a541db90b3d9fcacbbbfeec
Conner Reed <98499861+connerreed@users.noreply.github.com>
2023-11-24 22:59:08 -0600

    Image swiping almost complete
    Swiping animation is there, but needs some tuning.

--- reed-family/src/App.js
+++ reed-family/src/App.js
@@ -15,8 +15,8 @@ const recipeList = [
 ];
 
 const elementList = [
-  {id: 1, name: "Pictures", link: "#", list: pictureList}, // TODO: add picture list to object to display, and add link to picture landing page
-  {id: 2, name: "Recipes", link: "#", list: recipeList}, // TODO: add recipe list to object to display, and add link to recipe landing page
+  {id: 1, name: "Pictures", link: "#", images: pictureList}, // TODO: add picture list to object to display, and add link to picture landing page
+  {id: 2, name: "Recipes", link: "#", images: recipeList}, // TODO: add recipe list to object to display, and add link to recipe landing page
 ];
 
 
@@ -38,13 +38,13 @@ function App() {
   
 }
 
-function HomeElement({ element }) {
+/*function HomeElement({ element }) {
   const [currentPictureIndex, setCurrentPictureIndex] = useState(0); // Initializes currentPictureIndex to 0
   const [opacity, setOpacity] = useState(1); // Initializes opacity to 1, which is fully opaque
   const timerInterval = 5000; // milliseconds
 
   // useEffect hook runs after every render, and will re-run if element.list changes
-  /*useEffect(() => { // Sets up an interval that updates currentPictureIndex every 3 seconds. Index resets to 0 when it reaches the end of list, creating a loop
+  useEffect(() => { // Sets up an interval that updates currentPictureIndex every 3 seconds. Index resets to 0 when it reaches the end of list, creating a loop
     const timer = setInterval(() => {
       setCurrentPictureIndex(prevIndex => 
         prevIndex === element.list.length - 1 ? 0 : prevIndex + 1
@@ -53,9 +53,9 @@ function HomeElement({ element }) {
 
     return () => clearInterval(timer); // Clean up the interval on component unmount, prevents memory leaks
   }, [element.list]);
-  */
+  
 
-  useEffect(() => {
+  /*useEffect(() => {
     const interval = setInterval(() => {
       setOpacity(0); // Fade out
       setTimeout(() => {
@@ -81,5 +81,46 @@ function HomeElement({ element }) {
     </a>
   );  
 }
+*/
+
+const HomeElement = ({ element }) => {
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const [nextIdx, setNextIdx] = useState(1);
+  const [isSwiping, setIsSwiping] = useState(false); // New state to control the swipe
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsSwiping(true); // Start swiping
+
+      setTimeout(() => {
+        let newCurrentIdx = (currentIdx + 1) % element.images.length;
+        setCurrentIdx(newCurrentIdx);
+
+        let newNextIdx = (newCurrentIdx + 1) % element.images.length;
+        setNextIdx(newNextIdx);
+
+        setIsSwiping(false); // Reset swiping state after the swipe completes
+      }, 1000); // This should match your CSS transition duration
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [currentIdx, element.images.length]);
+
+  const currentPictureSrc = require(`${element.images[currentIdx].src}`);
+  const nextPictureSrc = require(`${element.images[nextIdx].src}`);
+
+  return (
+    <a href={element.link} className="card">
+      <div className="image-container">
+        <img src={currentPictureSrc} alt="Current" className={`swipe-img current ${isSwiping ? 'slide-left' : ''}`} />
+        <img src={nextPictureSrc} alt="Next" className={`swipe-img next ${isSwiping ? 'slide-left' : ''}`} />
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{element.name}</h5>
+      </div>
+    </a>
+  );
+};
+
 
 export default App;
