d4b32beb5fd055c4f44c3b7cb41a7536e9f87a0e
Conner Reed <connerdreed@gmail.com>
2023-12-05 16:33:32 -0600

    Added header navbar with some added styling

--- reed-family/src/App.js
+++ reed-family/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Slideshow from './Slideshow';
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 const pictureList = [ //TODO: Dynamically add pictures based on imgs/Pictures folder
   {id: 1, src: "./imgs/Pictures/FamilyPhoto1.png"},
@@ -24,9 +26,29 @@ const recipeList = [ //TODO: Dynamically add recipes based on imgs/Recipes folde
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <h1 id="title">Reed Family Website</h1>
-      </div>
+      <Navbar className="bg-body-tertiary" fixed="top" data-bs-theme="dark" bg="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Welcome Reed Family!</Navbar.Brand>
+        </Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <div className="container">
         <h1 className="slideshowLabel">Pictures</h1>
         <Slideshow className="slideshow" elementList={pictureList}/>
