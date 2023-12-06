1b97cee1b309342ee0d0754972c997b7549e16b6
Conner Reed <connerdreed@gmail.com>
2023-12-06 14:13:37 -0600

    Navbar collapse on element click added

--- reed-family/src/ReedNavbar.js
+++ reed-family/src/ReedNavbar.js
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 import "./ReedNavbar.css";
 function ReedNavbar() {
+	const [expanded, setExpanded] = useState(false); // state to allow for collapse onSelect of navbar links
+
 	return (
 		<Navbar
+			expanded={expanded}
 			fixed="top"
 			data-bs-theme="dark"
 			bg="dark"
 			expand="lg"
 			className="my-navbar"
+			collapseOnSelect
+			onToggle={setExpanded}
 		>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="me-auto">
-					<Link to="/" className="nav-link">
-						Home
-					</Link>
-					<Link to="/pictures" className="nav-link">
-						Pictures
-					</Link>
-					<Link to="/recipes" className="nav-link">
-						Recipes
-					</Link>
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav className="me-auto" onSelect={() => setExpanded(false)}>
+					<LinkContainer to="/">
+						<Nav.Link>Home</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to="/pictures">
+						<Nav.Link>Pictures</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to="/recipes">
+						<Nav.Link>Recipes</Nav.Link>
+					</LinkContainer>
 				</Nav>
 			</Navbar.Collapse>
 
