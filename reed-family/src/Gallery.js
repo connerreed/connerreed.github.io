186298c1af3d4b3a3a2dad1f83404927dbd99a87
Conner Reed <connerdreed@gmail.com>
2023-12-06 19:32:18 -0600

    Added magnifying effect to family picture gallery

--- reed-family/src/Gallery.js
+++ reed-family/src/Gallery.js
@@ -1,16 +1,24 @@
-import React from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Card, Button, Modal } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import "./Gallery.css";
 
 function Gallery({ elementList }) {
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+
+    const handleImageClick = (image) => {
+        setSelectedImage(image);
+        setShowModal(true);
+    }
+
 	return (
 		<Container>
 			<Row>
 				{elementList.map((element) => (
 					<Col md={4} sm={6} xs={12} key={element.id}>
-						<Card className="position-relative">
+						<Card className="position-relative" onClick={() => handleImageClick(element)} style={{cursor: 'pointer'}}>
 							<Card.Img
 								variant="top"
 								src={require(`${element.src}`)}
@@ -27,6 +35,14 @@ function Gallery({ elementList }) {
 					</Col>
 				))}
 			</Row>
+
+            <Modal show={showModal} onHide={() => setShowModal(false)} centered className="custom-modal">
+                <Modal.Body>
+                    {selectedImage && (
+                        <img src={require(`${selectedImage.src}`)} alt="Selected" className="img-fluid custom-modal-image"/>
+                    )}
+                </Modal.Body>
+            </Modal>
 		</Container>
 	);
 }
