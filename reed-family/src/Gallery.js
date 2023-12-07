ad4b734733172834e157e0e24ee7136485c77b22
Conner Reed <connerdreed@gmail.com>
2023-12-06 20:43:40 -0600

    Removed buttons and zoom on recipes

--- reed-family/src/Gallery.js
+++ reed-family/src/Gallery.js
@@ -4,45 +4,62 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import "./Gallery.css";
 
-function Gallery({ elementList }) {
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [showModal, setShowModal] = useState(false);
+function Gallery({ elementList, elementType }) {
+	const [selectedImage, setSelectedImage] = useState(null);
+	const [showModal, setShowModal] = useState(false);
 
-    const handleImageClick = (image) => {
-        setSelectedImage(image);
-        setShowModal(true);
-    }
+	const handleImageClick = (image) => {
+		if (elementType === "pictures") {
+			setSelectedImage(image);
+			setShowModal(true);
+		} else if (elementType === "recipes") {
+		}
+	};
 
 	return (
 		<Container>
 			<Row>
 				{elementList.map((element) => (
 					<Col md={4} sm={6} xs={12} key={element.id}>
-						<Card className="position-relative custom-card" onClick={() => handleImageClick(element)}>
+						<Card
+							className="position-relative custom-card"
+							onClick={() => handleImageClick(element)}
+						>
 							<Card.Img
 								variant="top"
 								src={require(`${element.src}`)}
 							/>
-							<Button
-								variant="primary"
-								href={require(`${element.src}`)}
-								download
-								className="download-button"
-							>
-								<FontAwesomeIcon icon={faDownload} />
-							</Button>
+							{elementType === "pictures" && (
+								<Button
+									variant="primary"
+									href={require(`${element.src}`)}
+									download
+									className="download-button"
+								>
+									<FontAwesomeIcon icon={faDownload} />
+								</Button>
+							)}
 						</Card>
 					</Col>
 				))}
 			</Row>
 
-            <Modal show={showModal} onHide={() => setShowModal(false)} centered className="custom-modal">
-                <Modal.Body>
-                    {selectedImage && (
-                        <img src={require(`${selectedImage.src}`)} alt="Selected" className="img-fluid custom-modal-image"/>
-                    )}
-                </Modal.Body>
-            </Modal>
+			<Modal
+				show={showModal}
+				onHide={() => setShowModal(false)}
+				centered
+				className="custom-modal"
+			>
+				<Modal.Body>
+					{selectedImage && (
+						<img
+							src={require(`${selectedImage.src}`)}
+							alt="Selected"
+							className="img-fluid custom-modal-image"
+						/>
+					)}
+				</Modal.Body>
+			</Modal>
 		</Container>
 	);
 }
