ed52cfa3934b5fc5cc1b572ab9442dc97f897646
Conner Reed <98499861+connerreed@users.noreply.github.com>
2023-12-15 14:51:15 -0600

    Code Styling

--- reed-family/src/RecipeDetail.js
+++ reed-family/src/RecipeDetail.js
@@ -1,36 +1,41 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 function RecipeDetail({ recipeList }) {
-	const { recipeFolderName } = useParams();
-	const [recipe, setRecipe] = useState(null);
+    const { recipeFolderName } = useParams();
+    const [recipe, setRecipe] = useState(null);
+    const navigate = useNavigate();
 
-	useEffect(() => {
-		// Find the recipe in the recipeList by id
-		const foundRecipe = recipeList.find(
-			(r) => r.folderName === recipeFolderName
-		);
-		setRecipe(foundRecipe);
-	}, [recipeFolderName, recipeList]);
+    useEffect(() => {
+        // Find the recipe in the recipeList by id
+        const foundRecipe = recipeList.find(
+            (r) => r.folderName === recipeFolderName
+        );
+        setRecipe(foundRecipe);
+    }, [recipeFolderName, recipeList]);
 
-	if (!recipe) {
-		return <div>Loading...</div>;
-	}
+    if (!recipe) {
+        return <div>Loading...</div>;
+    }
 
-	return (
-		<div>
-			<h1 style={{ color: "white" }}>{recipe.coverImg.name}</h1>
-			{/* Display the cover image */}
-			<img
-				src={recipe.coverImg.link}
-				alt={`Cover of Recipe ${recipe.coverImg.name}`}
-			/>
+    return (
+        <div>
+            <h1 style={{ color: "white" }}>{recipe.coverImg.name}</h1>
+            {/* Display the cover image */}
+            <img
+                src={recipe.coverImg.link}
+                alt={`Cover of Recipe ${recipe.coverImg.name}`}
+            />
 
-			{recipe.descriptionImgs.map((image, index) => (
-        <img key={index} src={image.link} alt={`Recipe Detail ${index + 1}`} />
-      ))}
-		</div>
-	);
+            {recipe.descriptionImgs.map((image, index) => (
+                <img
+                    key={index}
+                    src={image.link}
+                    alt={`Recipe Detail ${index + 1}`}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default RecipeDetail;
