862269bca0c1d7bce68e7b090a4288f95798345f
Conner Reed <98499861+connerreed@users.noreply.github.com>
2023-12-15 16:19:33 -0600

    Added PictureUploadForm to Picture Page
    Backend implementation not yet added to listen for POST

--- reed-family/src/Pictures.js
+++ reed-family/src/Pictures.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Gallery from './Gallery';
+import PictureUploadForm from './PictureUploadForm';
 
 function Pictures({pictureList}) {
     return(
-        <div>
+        <div style={{position: 'relative'}}>
+            <PictureUploadForm style={{top: '0', left: '0'}}/>
             <Gallery elementList={pictureList} elementType="pictures"/>
         </div>
     );
