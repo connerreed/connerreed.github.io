110c3ebc4f7788dbf6dde927dbdf19bdca57a61d
Conner Reed <98499861+connerreed@users.noreply.github.com>
2023-12-16 20:15:27 -0600

    Styled dropdown / Added some functionality
    Added loading message. Closed dropdown and cleared when submitted.

--- reed-family/src/UploadForm.jsx
+++ reed-family/src/UploadForm.jsx
@@ -1,38 +1,31 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef } from 'react';
 
-function CustomDropdown({ label, children }) {
-    const [isOpen, setIsOpen] = useState(false);
+function CustomDropdown({ label, children, isOpen, setIsOpen }) {
     const toggleRef = useRef(null);
 
     const toggleDropdown = () => setIsOpen(!isOpen);
 
     return (
-        <div style={{ position: "relative" }}>
+        <div style={{ position: 'relative' }}>
             <button
                 ref={toggleRef}
                 onClick={toggleDropdown}
-                style={{
-                    backgroundColor: "#28a745",
-                    color: "white",
-                    padding: "10px 15px",
-                    border: "none",
-                    borderRadius: "5px",
-                }}
+                style={{ backgroundColor: '#28a745', color: 'white', padding: '10px 15px', border: 'none', borderRadius: '5px' }}
             >
                 {`${label} ↓`}
             </button>
             {isOpen && (
                 <div
                     style={{
-                        position: "absolute",
-                        left: "50%",
-                        transform: "translateX(-50%)",
-                        width: "50%",
-                        backgroundColor: "#f8f9fa",
-                        borderColor: "#dee2e6",
-                        borderRadius: "0.25rem",
-                        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-                        paddingTop: "5px",
+                        position: 'absolute',
+                        left: '50%',
+                        transform: 'translateX(-50%)',
+                        width: '50%',
+                        backgroundColor: '#f8f9fa',
+                        borderColor: '#dee2e6',
+                        borderRadius: '0.25rem',
+                        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                        paddingTop: '5px',
                         zIndex: 1000,
                     }}
                 >
@@ -46,7 +39,8 @@ function CustomDropdown({ label, children }) {
 function UploadForm({ formType }) {
     const [selectedFiles, setSelectedFiles] = useState([]);
     const [recipeName, setRecipeName] = useState("");
-    const [successMessage, setSuccessMessage] = useState("");
+    const [message, setMessage] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
     const developMode = false;
 
     const handleFileChange = (event) => {
@@ -59,7 +53,9 @@ function UploadForm({ formType }) {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if (successMessage) setSuccessMessage(""); // Clear the success message if it's still showing
+        setMessage("Uploading... Please wait.");
+        setIsOpen(false); // Close the dropdown
+
         const formData = new FormData();
         selectedFiles.forEach((file) => {
             formData.append("files", file);
@@ -71,85 +67,41 @@ function UploadForm({ formType }) {
 
         try {
             const response = await fetch(
-                developMode
-                    ? `http://localhost:3001/upload/${formType}`
-                    : `https://reed-family-backend-b01b489ec3fe.herokuapp.com/upload/${formType}`,
+                developMode ? `http://localhost:3001/upload/${formType}` : `https://reed-family-backend-b01b489ec3fe.herokuapp.com/upload/${formType}`,
                 { method: "POST", body: formData }
             );
 
             if (response.ok) {
+                setMessage("Files uploaded successfully!");
                 setSelectedFiles([]);
                 setRecipeName("");
-                setSuccessMessage("Files uploaded successfully!");
-                setTimeout(() => setSuccessMessage(""), 3000);
+                setTimeout(() => setMessage(""), 3000);
             } else {
                 const errorData = await response.json();
-                console.error("Upload failed:", errorData.message);
-                alert("Failed to upload: " + errorData.message);
+                setMessage("Failed to upload: " + errorData.message);
             }
         } catch (error) {
-            console.error("Upload error:", error);
-            setSuccessMessage("Failed to upload: " + error.message);
+            setMessage("Failed to upload: " + error.message);
         }
     };
 
     return (
-        <div style={{ marginTop: "10px", textAlign: "center" }}>
-            <CustomDropdown
-                label={formType === "pictures" ? "Add Pictures" : "Add Recipes"}
-            >
-                <form onSubmit={handleSubmit} style={{ padding: "10px" }}>
+        <div style={{ marginTop: '10px', textAlign: 'center' }}>
+            <CustomDropdown label={formType === "pictures" ? "Add Pictures" : "Add Recipes"} isOpen={isOpen} setIsOpen={setIsOpen}>
+                <form onSubmit={handleSubmit} style={{ padding: '10px' }}>
                     {formType === "recipes" && (
-                        <div style={{ marginBottom: "10px" }}>
-                            <label
-                                htmlFor="recipeName"
-                                style={{ marginRight: "5px" }}
-                            >
-                                Recipe Name:
-                            </label>
-                            <input
-                                type="text"
-                                id="recipeName"
-                                value={recipeName}
-                                onChange={handleRecipeNameChange}
-                            />
+                        <div style={{ marginBottom: '10px' }}>
+                            <label htmlFor="recipeName" style={{ marginRight: '5px' }}>Recipe Name:</label>
+                            <input type="text" id="recipeName" value={recipeName} onChange={handleRecipeNameChange} />
                         </div>
                     )}
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            marginBottom: "10px",
-                        }}
-                    >
-                        <button
-                            type="button"
-                            onClick={() =>
-                                document.getElementById("fileInput").click()
-                            }
-                            style={{ marginBottom: "10px" }}
-                        >
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: '10px' }}>
+                        <button type="button" onClick={() => document.getElementById("fileInput").click()} style={{ marginBottom: '10px' }}>
                             Add Picture(s)
                         </button>
-                        <input
-                            type="file"
-                            id="fileInput"
-                            style={{ display: "none" }}
-                            onChange={handleFileChange}
-                            multiple
-                        />
+                        <input type="file" id="fileInput" style={{ display: "none" }} onChange={handleFileChange} multiple />
                         {selectedFiles.map((file, index) => (
-                            <div
-                                key={index}
-                                style={{
-                                    overflow: "hidden",
-                                    textOverflow: "ellipsis",
-                                    whiteSpace: "nowrap",
-                                    maxWidth: "90%",
-                                    marginBottom: "5px",
-                                }}
-                            >
+                            <div key={index} style={{ overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap", maxWidth: "90%", marginBottom: '5px' }}>
                                 {file.name}
                             </div>
                         ))}
@@ -157,10 +109,8 @@ function UploadForm({ formType }) {
                     </div>
                 </form>
             </CustomDropdown>
-            {successMessage && (
-                <div style={{ color: "white", marginTop: "10px" }}>
-                    {successMessage}
-                </div>
+            {message && (
+                <div style={{ color: "white", marginTop: '10px' }}>{message}</div>
             )}
         </div>
     );
