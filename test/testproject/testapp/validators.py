dee1ae5e5cabef14b670d9c7296755db29337c1a
Conner Reed <98499861+connerreed@users.noreply.github.com>
2024-08-14 12:03:41 -0500

    Delete test django project that was used for debugging

--- test/testproject/testapp/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth import get_user_model
-
-
-def validate_unique_email(value):
-    User = get_user_model()
-    if User.objects.filter(email=value).exists():
-        raise ValidationError(_('User with this email already exists'), code='unique')
