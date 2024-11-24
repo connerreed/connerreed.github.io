af59b19c95b70f55bcf96c884bb2c46ca11a2ad4
Conner Reed <98499861+connerreed@users.noreply.github.com>
2024-11-24 11:55:50 -0600

    In Progress - Styling Navbar

--- frontend/src/components/CustomNavbar.jsx
+++ frontend/src/components/CustomNavbar.jsx
@@ -1,39 +1,78 @@
 import React from "react";
-import { Navbar, Nav, Row, Col } from "react-bootstrap";
+import { Navbar, Nav, Offcanvas } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import { useLocation, useNavigate } from "react-router-dom";
-import "../css/CustomNavbar.css"; // Import custom CSS
-import Button from "react-bootstrap/Button";
-import { useAuth } from "../hooks/AuthContext";
+import {useNavigate, useLocation} from "react-router-dom";
+import { Row, Col, Button } from "react-bootstrap";
+//import { useAuth } from "../hooks/AuthContext";
 import { useTheme } from "../hooks/ThemeContext";
-//import ProfileIcon from "./ProfileIcon";
 
 function CustomNavbar() {
-    const location = useLocation();
     const navigate = useNavigate();
-    const { authToken } = useAuth();
+    const location = useLocation();
+    //const { authToken } = useAuth();
     const { darkMode } = useTheme();
 
     const showBackButton = !/^\/(recipes|pictures|videos|login|profile|)$/.test(
-        // Everything besides the navigation pages
         location.pathname
     );
-
-    // TODO: Make elements in the navbar collapse one by one
 
     return (
         <>
             <Navbar
-                expand="lg"
                 variant={darkMode ? "dark" : "light"}
                 className="navbar-container"
+                expand="lg"
+                collapseOnSelect
             >
+                <Navbar.Toggle aria-controls="offcanvasNavbar" />
                 <Navbar.Brand onClick={() => navigate("/")}>
                     <h1 className="navbar-title">Reed Family</h1>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto"/>
-                <Nav className="ms-auto profile-container">
-                    <LinkContainer to={authToken ? "/profile" : "/login"}>
+                <Navbar.Offcanvas
+                    id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel"
+                    placement="start"
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id="offcanvasNavbarLabel">
+                            Menu
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-center flex-grow-1">
+                            <LinkContainer
+                                to="/"
+                                id="nav-home"
+                                className="navbar-element"
+                            >
+                                <Nav.Link>Home</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer
+                                to="/recipes"
+                                id="nav-recipes"
+                                className="navbar-element"
+                            >
+                                <Nav.Link>Recipes</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer
+                                to="/pictures"
+                                id="nav-pictures"
+                                className="navbar-element"
+                            >
+                                <Nav.Link>Pictures</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer
+                                to="/videos"
+                                id="nav-videos"
+                                className="navbar-element"
+                            >
+                                <Nav.Link>Videos</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+                <Nav className="profile-container">
+                    <LinkContainer to="/profile" id="nav-profile">
                         <Nav.Link>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -43,27 +82,15 @@ function CustomNavbar() {
                                 className="bi bi-person"
                                 viewBox="0 0 16 16"
                             >
-                                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                <path
+                                    fillRule="evenodd"
+                                    d="M8 9a6 6 0 0 0-6 6v1h12v-1a6 6 0 0 0-6-6z"
+                                />
                             </svg>
                         </Nav.Link>
                     </LinkContainer>
                 </Nav>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto navbar-elements">
-                        <LinkContainer to="/">
-                            <Nav.Link>Home</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/recipes">
-                            <Nav.Link>Recipes</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/pictures">
-                            <Nav.Link>Pictures</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/videos">
-                            <Nav.Link>Videos</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
             </Navbar>
             {showBackButton && (
                 <Row>
