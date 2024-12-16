15d532768788c9f864fcd849a4c85407d127cc72
Conner Reed <98499861+connerreed@users.noreply.github.com>
2024-12-15 20:49:00 -0600

    Fixed login bug
    It would load then display "Something went wrong" for a sec then showed profile normally. This is now fixed

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { useTheme } from "../contexts/ThemeContext";
 import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Profile = () => {
     const { logout, userData, authToken } = useAuth();
     const { darkMode, setDarkMode } = useTheme();
-    //const [user, setUser] = useState(null);
-    //const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     const toggleDarkMode = async () => {
@@ -28,58 +25,18 @@ const Profile = () => {
         }
     };
 
-    /*useEffect(() => {
-        const fetchUser = async () => {
-            setLoading(true);
-            try {
-                const response = await axios.get("http://127.0.0.1:8000/auth/users/me/", {
-                    headers: { Authorization: `Token ${authToken}` },
-                });
-                setUser(response.data);
-            } catch (error) {
-                console.error("User fetch error: ", error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        if (authToken) {
-            fetchUser();
-        }
-    }, [authToken]);
-    */
-
     const handleLogout = () => {
         logout();
         // Optionally, redirect to login
         navigate("/login");
     };
 
-    /*if (loading) {
-        return (
-            <Container className="d-flex justify-content-center align-items-center vh-100">
-                <Spinner animation="border" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </Spinner>
-            </Container>
-        );
-    }
-    */
-
-    if (!userData) { // FIXME: on login this shows up briefly
-        return (
-            <Container className="d-flex justify-content-center align-items-center vh-100">
-                <p>Something went wrong when loading user data...</p>
-            </Container>
-        );
-    }
-
     return (
         <div>
             <h1>
-                {userData.first_name} {userData.last_name}
+                {userData?.first_name} {userData?.last_name}
             </h1>
-            <p>Email: {userData.email}</p>
+            <p>Email: {userData?.email}</p>
             <Button onClick={handleLogout}>Logout</Button>
             <Button onClick={toggleDarkMode}>Toggle Dark Mode</Button>
         </div>
