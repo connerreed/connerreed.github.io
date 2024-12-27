d485c8d0f7c8a454b46a9fbb8f4332dc76cf6dab
Conner Reed <98499861+connerreed@users.noreply.github.com>
2024-12-26 20:57:01 -0600

    Recipe content page displays all information
    Added styling with desktop/mobile breakpoints for render size

--- frontend/src/components/Recipe.jsx
+++ frontend/src/components/Recipe.jsx
@@ -1,11 +1,5 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import CardGroup from "react-bootstrap/CardGroup";
-import ListGroup from "react-bootstrap/ListGroup";
 import Loading from "./Loading";
 import backendURL from "../utils/backendURL";
 import useApiRequest from "../hooks/useApiRequest";
@@ -28,86 +22,63 @@ const Recipe = () => {
         return <Loading />;
     }
 
-    return (
-        <Container className="mt-5">
-            <Row>
-                <Col>
-                    <Card data-bs-theme="dark" className="mb-3 mt-3">
-                        <Card.Body>
-                            <Card.Title>
-                                <h1>{recipe?.title}</h1>
-                            </Card.Title>
-                            <Card.Subtitle className="mt-3">
-                                <h3>
-                                    Recipe Author: {recipe?.recipeAuthor} &emsp;
-                                    Uploaded By: {recipe?.user.first_name}{" "}
-                                    {recipe?.user.last_name}
-                                </h3>
-                            </Card.Subtitle>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+    const dateUploaded = new Date(recipe?.date_uploaded);
+    const dayUploaded = dateUploaded.getDate();
+    const monthUploaded = dateUploaded.getMonth() + 1;
+    const yearUploaded = dateUploaded.getFullYear();
 
-            {recipe?.description !== "" && (
-                <Row>
-                    <Col>
-                        <Card bg="secondary" className="mb-3">
-                            <Card.Body>
-                                <CardGroup>
-                                    <Card.Title>Description</Card.Title>
-                                </CardGroup>
-                                <CardGroup>
-                                    <Card.Text>{recipe?.description}</Card.Text>
-                                </CardGroup>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            )}
-            <Row>
-                <Col>
-                    <Card bg="secondary" className="mb-3">
-                        <ListGroup variant="flush" data-bs-theme="dark">
-                            <ListGroup.Item>
-                                <Card.Title>
-                                    <strong>{recipe?.title}</strong>
-                                </Card.Title>
-                            </ListGroup.Item>
-                            <ListGroup.Item>Test</ListGroup.Item>
-                        </ListGroup>
-                        <Card.Body>
-                            <Card.Title>
-                                <strong>{recipe?.title}</strong>
-                            </Card.Title>
-                            <Card.Subtitle className="text-muted">
-                                Uploaded By:{" "}
-                                {recipe?.user.first_name +
-                                    " " +
-                                    recipe?.user.last_name}{" "}
-                                <br />
-                                Recipe Source: {recipe?.recipeAuthor}
-                            </Card.Subtitle>
-                            {recipe?.description !== "" && (
-                                <Card.Text>
-                                    <u>Description</u>: {recipe?.description}
-                                </Card.Text>
-                            )}
-                        </Card.Body>
-                        <Card.Img variant="top" src={recipe?.thumbnail} />
-                    </Card>
-                </Col>
-            </Row>
-            <Row>
-                {recipe?.images?.map((image, index) => (
-                    <Col md={4} className="mb-4" key={index}>
-                        <Card bg="secondary" className="mt-3 shadow-sm">
-                            <Card.Img variant="top" src={image.image} />
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
-        </Container>
+    return (
+        <div className="d-flex justify-content-center">
+            <div className="recipe-content-container col-lg-4 col-md-6 col-sm-10">
+                <div>
+                    <h1 className="text-center">
+                        <strong>{recipe?.title}</strong>
+                    </h1>
+                </div>
+                <div className="mt-3">
+                    <div>
+                        Recipe Author: <strong>{recipe?.recipeAuthor}</strong>{" "}
+                    </div>
+                    <div className="d-inline-block">
+                        Uploaded By:{" "}
+                        <strong>
+                            {recipe?.user.first_name} {recipe?.user.last_name}
+                        </strong>{" "}
+                        |
+                    </div>{" "}
+                    <div className="d-inline-block">
+                        Date Uploaded: {monthUploaded}/{dayUploaded}/
+                        {yearUploaded}
+                    </div>
+                </div>
+                {recipe?.description !== "" && (
+                    <div className="mt-3">
+                        <p>{recipe?.description}</p>
+                    </div>
+                )}
+                <div className="mt-3">
+                    <img
+                        className="w-100 h-auto"
+                        src={recipe?.thumbnail}
+                        alt="Recipe Thumbnail"
+                    />
+                </div>
+                {recipe?.serialized_images?.length > 0 && (
+                    <div className="mt-3">
+                        {recipe.serialized_images.map((image, index) => {
+                            return (
+                                <img 
+                                    key={index}
+                                    className="w-100 mb-3 h-auto"
+                                    src={image.image}
+                                    alt={`${image.recipe_title} Recipe`}
+                                />
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
+        </div>
     );
 };
 
